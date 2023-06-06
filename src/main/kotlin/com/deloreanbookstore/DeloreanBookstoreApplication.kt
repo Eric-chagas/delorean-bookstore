611467dbafa1390142804cb2f6bf43438672cce9
Eric-chagas <eric_chagas12@outlook.com>
2023-06-05 21:42:23 -0300

    Initial commit and spring boot files

---
+++ src/main/kotlin/com/deloreanbookstore/DeloreanBookstoreApplication.kt
@@ -0,0 +1,12 @@
+package com.deloreanbookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeloreanBookstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeloreanBookstoreApplication>(*args)
+    print("hello fri3nd")
+}
